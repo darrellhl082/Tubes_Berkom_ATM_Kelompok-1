7161535cacc521c92bca8ea91540c89480890e60
darrellhl082 <darrellhl082@gmail.com>
2024-11-04 21:05:59 +0700

    Update Template
    Memperbaharui Template dan stuktur Program

--- main.py
+++ main.py
@@ -4,43 +4,34 @@
 # Kamus
 
 # Variabel
-# cek_lanjut: str
 # main_loop: bool
 
 # Fungsi
-# Hello(): Print Hello
+
 
 # Algoritma
 #
 
 # Import fungsi
-from module.hello import hello
+# from module.hello import hello
 
+
+# Definisi Variabel dan Array
 
 # Definisi Fungsi
+
 def main():
     main_loop = True
-    cek_lanjut : str
 
     while main_loop:
 
-        hello()
-
-       
-
-        print(ranti)
-
-        print("zaki")
+        # Kondisi Menu
+    
 
 
-        print("odre")
-
-
-        print ("Hellow world")
+    
+        
         # Loop Termination
-        cek_lanjut = input("Lanjut? Ya/Tidak: ") 
-        if cek_lanjut == "Tidak":
-            main_loop = False
         
         if not main_loop: # Loop Terminator
             break
