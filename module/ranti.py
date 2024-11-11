b411356e827a1645b20476e7778e50b5cf6b80b1
rkhairanti <rkhairanti@gmail.com>
2024-11-11 14:29:29 +0700

    update penarikan
    nambah nasabah saat ini dan update saldo nasabah saat ini

--- module/ranti.py
+++ module/ranti.py
@@ -1,47 +1,73 @@
 # Program Fungsi Penarikan ATM
-# Python
 
-def penarikan_tunai():
-    print("PENARIKAN TUNAI")
-    """
-    50000
-    250000
-    500000
-    750000
-    1000000
-    1250000
-    1500000
-    """
-    nominal = ["50000", "250000", "500000", "750000", "1000000", "1250000", "1500000"]
-    for data in nominal:
-        print(data)
-penarikan_tunai()
-
-def penarikan_jumlah_lain():
-    print("PENARIKAN JUMLAH LAIN")
-    print("MASUKKAN JUMLAH UANG YANG AKAN DITARIK")
-
-saldo = 5000000
+# saldo = 5000000
 min_saldo = 50000
-
-while True:
-    penarikan_jumlah_lain()
-    jumlah_tarik = int(input("Rp"))
-    pilih = input("1. TEKAN 1 JIKA BENAR \n2. TEKAN 2 JIKA KOREKSI\n")
-    if pilih == "1":
-        if jumlah_tarik > saldo:
-            print("SALDO ANDA TIDAK MENCUKUPI")
-        elif saldo < min_saldo:
-            print("SALDO ANDA TIDAK MENCUKUPI")
-        elif saldo-jumlah_tarik < min_saldo:
-            print("TRANSAKSI GAGAL")
-            print("SALDO ANDA TIDAK MENCUKUPI")
-        else:
-            print("TRANSAKSI BERHASIL")
-            print("SILAHKAN MENGAMBIL UANG ANDA")
-            saldo = saldo - jumlah_tarik
-            print("SISA SALDO ANDA", saldo)
-            break
-    elif pilih == "2":
-        # koreksi jumlah penarikan yang ingin diambil, terus masukin ulang lagi
-        pass
+def penarikan_tunai(nasabah_now, jumlah_penarikan):
+    while True:
+        if jumlah_penarikan == None:     
+            jumlah_penarikan = int(input("Rp"))
+            pilih = input("1. TEKAN 1 JIKA BENAR \n2. TEKAN 2 JIKA KOREKSI\n")
+            if pilih == 2:
+                pass
+            if jumlah_penarikan > nasabah_now["saldo"]:
+                print(
+                """
+                __________________________________________
+                |                                        |
+                |                                        |
+                |                                        |
+                |                                        |
+                |       SALDO ANDA TIDAK MENCUKUPI       |
+                |                                        |
+                |                                        |
+                |                                        |
+                |________________________________________|
+                """
+                )
+            elif nasabah_now < min_saldo:
+                print(
+                """
+                __________________________________________
+                |                                        |
+                |                                        |
+                |                                        |
+                |                                        |
+                |       SALDO ANDA TIDAK MENCUKUPI       |
+                |                                        |
+                |                                        |
+                |                                        |
+                |________________________________________|
+                """
+                )
+            elif nasabah_now-jumlah_penarikan < min_saldo:
+                print(
+                """
+                __________________________________________
+                |                                        |
+                |                                        |
+                |                                        |
+                |            TRANSAKSI GAGAL             |
+                |       SALDO ANDA TIDAK MENCUKUPI       |
+                |                                        |
+                |                                        |
+                |                                        |
+                |________________________________________|
+                """
+                )
+            else:
+                nasabah_now = nasabah_now - jumlah_penarikan
+                print(
+                f"""
+                __________________________________________
+                |                                        |
+                |                                        |
+                |            TRANSAKSI BERHASIL          |
+                |      SILAHKAN MENGAMBIL UANG ANDA      |
+                |            TERIMA KASIH :)             |
+                |                                        |
+                |      SISA SALDO ANDA {nasabah_now}     |
+                |                                        |
+                |________________________________________|
+                """
+                )
+                break
