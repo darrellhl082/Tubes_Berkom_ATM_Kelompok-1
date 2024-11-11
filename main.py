f1fd2f18035ab62dc4b834b6505b11b907a550c0
Yuzakin <105656648+Yuzakin@users.noreply.github.com>
2024-11-11 12:10:46 +0700

    Add Menu
    Kode untuk menu awal ATM, dibuat dalam main.py.

--- main.py
+++ main.py
@@ -13,6 +13,7 @@
 #
 
 # Import fungsi
+
 # from module.hello import hello
 
 
@@ -20,13 +21,61 @@
 
 # Definisi Fungsi
 
+def penarikan_tunai(jumlah):
+    print(jumlah)
+
 def main():
     main_loop = True
 
     while main_loop:
 
         # Kondisi Menu
-    
+        print(
+        """
+        __________________________________________
+        |                                        |
+        |            PENARIKAN TUNAI             |
+        | SILAHKAN MASUKKAN ANGKA SESUAI PILIHAN |
+        |                                        |
+        | (1) 50.000      (5) 1.000.000          |
+        | (2) 250.000     (6) 1.500.000          |
+        | (3) 500.000     (7) Jumlah Lainnya     |
+        | (4) 750.000     (8) Transaksi Lainnya  |
+        |________________________________________|
+        """
+        )
+        input_pilihan_menu = int(input("Pilih menu: "))
+
+        list_nominal = [50000, 250000, 500000, 750000, 1000000, 1500000, None]
+        if input_pilihan_menu < 7:
+            penarikan_tunai(list_nominal[input_pilihan_menu - 1])
+        elif input_pilihan_menu == 8:
+            print(
+        """
+        __________________________________________
+        |                                        |
+        |             TRANSAKSI LAIN             |
+        | SILAHKAN MASUKKAN ANGKA SESUAI PILIHAN |
+        |                                        |
+        | (1) INFORMASI SALDO                    |
+        | (2) TRANSFER ANTAR BANK                |
+        | (3) PENARIKAN TUNAI                    |
+        |                                        |
+        |________________________________________|
+        """
+            )
+            input_pilihan_menu = int(input("Pilih menu: "))
+            while True:           
+                if input_pilihan_menu == 1:
+                    print("informasi_saldo()")
+                    break
+                elif input_pilihan_menu == 2:
+                    print("transfer bank")
+                    break
+                elif input_pilihan_menu == 3:
+                    break
+                else:
+                    input_pilihan_menu = int(input("Pilih menu: "))
 
 
     
