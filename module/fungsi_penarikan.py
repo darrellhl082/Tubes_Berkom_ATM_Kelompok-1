d6061da60383e24e23eea8bf3f09a6769e2c3c69
Yuzakin <105656648+Yuzakin@users.noreply.github.com>
2024-11-11 15:38:50 +0700

    Fix fungsi_penarikan.py
    Delete test case, fix few output formatting

--- module/fungsi_penarikan.py
+++ module/fungsi_penarikan.py
@@ -1,5 +1,4 @@
 # Program Fungsi Penarikan ATM
-nasabah_now = {"saldo": 500000}
 # saldo = 5000000
 min_saldo = 50000
 def penarikan_tunai(nasabah_now, jumlah_penarikan):
@@ -14,66 +13,66 @@ def penarikan_tunai(nasabah_now, jumlah_penarikan):
             pilih = input("1. TEKAN 1 JIKA BENAR \n2. TEKAN 2 JIKA KOREKSI\n")
             if pilih == 2:
                 pass
-            if jumlah_penarikan > nasabah_now["saldo"]:
-                print(
-                """
-                __________________________________________
-                |                                        |
-                |                                        |
-                |                                        |
-                |                                        |
-                |       SALDO ANDA TIDAK MENCUKUPI       |
-                |                                        |
-                |                                        |
-                |                                        |
-                |________________________________________|
-                """
-                )
-            elif nasabah_now["saldo"] < min_saldo:
-                print(
-                """
-                __________________________________________
-                |                                        |
-                |                                        |
-                |                                        |
-                |                                        |
-                |       SALDO ANDA TIDAK MENCUKUPI       |
-                |                                        |
-                |                                        |
-                |                                        |
-                |________________________________________|
-                """
-                )
-            elif nasabah_now["saldo"] - jumlah_penarikan < min_saldo:
-                print(
-                """
-                __________________________________________
-                |                                        |
-                |                                        |
-                |                                        |
-                |            TRANSAKSI GAGAL             |
-                |       SALDO ANDA TIDAK MENCUKUPI       |
-                |                                        |
-                |                                        |
-                |                                        |
-                |________________________________________|
-                """
-                )
-            else:
-                nasabah_now["saldo"] -= jumlah_penarikan
-                saldo_text = f"SISA SALDO ANDA {nasabah_now["saldo"]}".center(40)
-                print(
-                f"""
-                __________________________________________
-                |                                        |
-                |                                        |
-                |            TRANSAKSI BERHASIL          |
-                |      SILAHKAN MENGAMBIL UANG ANDA      |
-                |            TERIMA KASIH :)             |
-                |                                        |
-                |{saldo_text}|
-                |                                        |
-                |________________________________________|
-                """
-                )
-                break
+        if jumlah_penarikan > nasabah_now["saldo"]:
+            print(
+        """
+        __________________________________________
+        |                                        |
+        |                                        |
+        |                                        |
+        |                                        |
+        |       SALDO ANDA TIDAK MENCUKUPI       |
+        |                                        |
+        |                                        |
+        |                                        |
+        |________________________________________|
+        """
+            )
+        elif nasabah_now["saldo"] < min_saldo:
+            print(
+        """
+        __________________________________________
+        |                                        |
+        |                                        |
+        |                                        |
+        |                                        |
+        |       SALDO ANDA TIDAK MENCUKUPI       |
+        |                                        |
+        |                                        |
+        |                                        |
+        |________________________________________|
+        """
+            )
+        elif nasabah_now["saldo"] - jumlah_penarikan < min_saldo:
+            print(
+        """
+        __________________________________________
+        |                                        |
+        |                                        |
+        |                                        |
+        |            TRANSAKSI GAGAL             |
+        |       SALDO ANDA TIDAK MENCUKUPI       |
+        |                                        |
+        |                                        |
+        |                                        |
+        |________________________________________|
+        """
+            )
+        else:
+            nasabah_now["saldo"] -= jumlah_penarikan
+            saldo_text = f"SISA SALDO ANDA {nasabah_now["saldo"]}".center(40)
+            print(
+        f"""
+        __________________________________________
+        |                                        |
+        |                                        |
+        |            TRANSAKSI BERHASIL          |
+        |      SILAHKAN MENGAMBIL UANG ANDA      |
+        |            TERIMA KASIH :)             |
+        |                                        |
+        |{saldo_text}|
+        |                                        |
+        |________________________________________|
+        """
+            )
+            break
