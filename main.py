46306d85f5a168f5fe51cde7ce68301ef9a0c68c
Yuzakin <105656648+Yuzakin@users.noreply.github.com>
2024-11-16 21:44:55 +0700

    Update main.py
    Added KAMUS, comments, etc.
    Optimized line 79 onwards (input and function calling for second menu section)

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
 # Program Simulasi ATM
 # Lorem Ipsum
 
-# Kamus
-
-# Variabel
-# main_loop: bool
-
-# Fungsi
-
+# KAMUS
+# -
 
 # Algoritma
-#
 
-# Import fungsi
+# Import fungsi dan prosedur dari berbagai file serta module
 from module.data_nasabah import data_nasabah
 from module.main_autentikasi import Main_Otentikasi # fungsi autentikasi
 from module.cek_saldo import cek_saldo # fungsi cek saldo
@@ -20,17 +14,24 @@ from module.fungsi_penarikan import penarikan_tunai
 from module.transfer import transfer
 from module import main_loop_config
 from time import sleep
-# Definisi Variabel dan Array
 
-# Definisi Fungsi
-
+# Definisi prosedur
 def main():
-    main_loop_config.main_loop = True
-    nasabah_now = Main_Otentikasi(data_nasabah)
 
-    while main_loop_config.main_loop:
-        sleep(1.5)
-        
+    # Program bagian utama pada ATM yang akan menampilkan menu dan mengeksekusi fungsi/prosedur pada ATM secara berulang hingga diberhentikan
+
+    # KAMUS LOKAL
+    # saldo_text: str; jumlah saldo (dicenter dalam kotak)
+    # input_pilihan_menu: str; memegang input pilihan menu
+    # nasabah_now: dict; memegang data pengguna
+    # list_nominal: list of integers; nominal-nominal penarikan
+
+    main_loop_config.main_loop = True # Inisialisasi untuk memastikan prosedur akan diulang hingga diberhentikan oleh pengguna
+    nasabah_now = Main_Otentikasi(data_nasabah) # Memegang data pengguna dalam variabel nasabah_now
+    sleep(1.0) # Menunda eksekusi kode line selanjutnya selama x (1.5) detik
+
+    while main_loop_config.main_loop: # Pengulangan jika main_loop pada file main_loop_config.py bernilai True
+        sleep(0.5)
         # Kondisi Menu
         print(
         """
@@ -45,7 +46,9 @@ def main():
         | (4) 750.000     (8) TRANSAKSI LAINNYA  |
         |________________________________________|
         """
-        )
+        ) # Penampilan awal menu
+
+        # Rangkaian instruksi meminta input pengguna hingga terdapat input yang ada opsinya
         input_pilihan_menu = int(input("Pilih menu: "))
         while True:  
             if input_pilihan_menu < 0 or input_pilihan_menu > 8:
@@ -54,11 +57,13 @@ def main():
                 break
 
         sleep(0.5)
-        list_nominal = [50000, 250000, 500000, 750000, 1000000, 1500000, None]
+        list_nominal = [50000, 250000, 500000, 750000, 1000000, 1500000, None] # List opsi nominal penarikan
 
-        # Penarikan
+        # Pemberhentian program jika input = 0
         if input_pilihan_menu == 0:
             main_loop_config.main_loop = False
+
+        # Penarikan, memanggil fungsi penarikan_tunai dan menggunakan nominal pada list_nominal yang sesuai
         elif input_pilihan_menu < 8:
             penarikan_tunai(nasabah_now, list_nominal[input_pilihan_menu - 1])
         
@@ -77,30 +82,26 @@ def main():
         | (3) PENARIKAN TUNAI                    |
         |________________________________________|
         """
-            )
+            ) # Penampilan menu kedua
+
+            # Rangkaian instruksi meminta input pengguna hingga terdapat input yang ada opsinya
             input_pilihan_menu = int(input("Pilih menu: "))
-            sleep(0.5)
-
-            while True:
-                if input_pilihan_menu == 0:
-                    main_loop_config.main_loop = False
-                    break
-                if input_pilihan_menu == 1:
-                    cek_saldo(nasabah_now)
-                    break
-                elif input_pilihan_menu == 2:
-                    transfer(data_nasabah, nasabah_now)
-                    break
-                elif input_pilihan_menu == 3:
-                    break
-                else:
+            while True:  
+                if input_pilihan_menu < 0 or input_pilihan_menu > 3:
                     input_pilihan_menu = int(input("Opsi tidak ditemukan. Pilih menu: "))
-
-
-    
+                else:
+                    break
+            sleep(0.5)
+            
+            if input_pilihan_menu == 0: # Jika di cancel, memberhentikan perulangan 
+                main_loop_config.main_loop = False
+            elif input_pilihan_menu == 1: 
+                cek_saldo(nasabah_now) # Cek saldo, memanggil fungsi cek_saldo dan menggunakan data pengguna 
+            elif input_pilihan_menu == 2:
+                transfer(data_nasabah, nasabah_now) # Transfer, memanggil fungsi transfer dan menggunakan data pengguna serta database pengguna ATM
+                
         
-        # Loop Termination
-        
+        # Loop Termination        
         if not main_loop_config.main_loop: # Loop Terminator
             print(
         """
@@ -115,18 +116,10 @@ def main():
         |                                        |
         |________________________________________|
         """
-            )
+            ) # Penampilan sebelum keluar program
             sleep(1.5)
             break
 
 
-
-# Main Loop
+# Eksekusi Main Loop
 main()
-
-
-
-
-
-
-
